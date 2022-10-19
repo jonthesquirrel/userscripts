755da4c2b307306cb2b95472a73f0f69bb1977af
Jonathan Herman <jdh11235@gmail.com>
2022-10-18 21:50:10 -0500

    Create AutoHermitCraftTabClicker.user.js

---
+++ AutoHermitCraftTabClicker.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Auto HermitCraft Tab Clicker for hermitcraft.com
+// @namespace    https://jonthesquirrel.github.io
+// @version      0.1
+// @description  try to take over the world!
+// @author       jonthesquirrel
+// @match        https://hermitcraft.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hermitcraft.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector("#btnHermitCraft").click()
+})();
